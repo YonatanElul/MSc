4ae23f8309ac5d899dc2389d7d6bce52b388adbe
Yonatan Elul <renedal@gmail.com>
2018-10-15 05:57:52 +0300

    Finished Implementing the ModeLoss

--- DL/Losses/ModeLoss.py
+++ DL/Losses/ModeLoss.py
@@ -1,11 +1,23 @@
 from DL.Losses.WassersteinLoss import improved_wasserstein_loss
+from DL.Utilities.MathOps import auto_corr2d
 
 import tensorflow as tf
 import numpy as np
 
 
-def corr_loss(true_input, model_input):
-    true_ac = np.random.
+def corr_loss(true_input: np.ndarray, model_input: np.ndarray):
+    """
+
+    :param true_input:
+    :param model_input:
+    :return:
+    """
+
+    true_ac = auto_corr2d(in_arr=true_input, kernel=(3, 3), stride=(2, 2), mode='valid')
+    model_ac = auto_corr2d(in_arr=model_input, kernel=(3, 3), stride=(2, 2), mode='valid')
+    ac_loss = np.mean(np.abs(true_ac - model_ac))
+
+    return ac_loss
 
 
 def mode_loss(discriminator, real_data: tf.Tensor, fake_data: tf.Tensor,
@@ -33,3 +45,7 @@ def mode_loss(discriminator, real_data: tf.Tensor, fake_data: tf.Tensor,
                                                                        g_penalty_lambda=g_penalty_lambda)
 
     # Compute the the Correlative loss
+    ac_loss = tf.py_func(corr_loss, [real_data, fake_data])
+    gen_loss = w_generator_loss + gamma * ac_loss
+
+    return gen_loss, w_discriminator_loss
--- DL/Utilities/MathOps.py
+++ DL/Utilities/MathOps.py
@@ -1,4 +1,21 @@
+from scipy.signal import correlate2d
+
 import numpy as np
 
 
-def corr2d(mat1: np.ndarray, mat2: np.ndarray):
+def auto_corr2d(in_arr: np.ndarray, kernel: (int, int) = (3, 3), stride: (int, int) = (2, 2), mode: str = 'valid'):
+    M = in_arr.shape[0]
+    N = in_arr.shape[1]
+
+    for i in range(0, M - kernel[0], stride[0]):
+        for j in range(0, N - kernel[1], stride[1]):
+            local_arr = in_arr[i:(i + kernel[0]), j:(j + kernel[1])]
+
+            if i == 0 and j == 0:
+                ac = np.expand_dims(correlate2d(local_arr, in_arr, mode=mode), axis=2)
+
+            else:
+                ac = np.concatenate((ac, np.expand_dims(correlate2d(local_arr, in_arr, mode=mode), axis=2)), axis=2)
+
+    return ac
+
